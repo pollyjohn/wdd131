408c486232f0e4b34d87b9e65f7fdd65df4cdd7f
pollyjohn <joaotv16@gmail.com>
2025-09-27 03:26:29 -0300

    definitive temples.css and temples-large.css

--- week2/media-query.js
+++ week2/media-query.js
@@ -5,3 +5,25 @@ hambuttom.addEventListener('click', () => {
     mainnav.classList.toggle('show');
     hambuttom.classList.toggle('show');
 })
+
+const year = document.querySelector("#currentyear")
+const lastModifiedElement = document.getElementById("lastModified");
+
+const today = new Date();
+
+
+const formatter = new Intl.DateTimeFormat("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric"
+  });
+
+
+const parsedDateTime = formatter.format(today);
+
+
+currentyear.innerHTML = `<span >${today.getFullYear()}</span>`;
+lastModifiedElement.innerHTML = `<span class="lastModified">Last Modified: ${parsedDateTime}</span>`;
